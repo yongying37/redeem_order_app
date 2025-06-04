21f1a4f64b1e7ab56da35fc5f46ef3afde4775f9
2402212-zixin <2402212@sit.singaporetech.edu.sg>
2025-06-04 18:39:37 +0800

    Separate Nav Bar into a file

---
+++ lib/widgets/custom_bottom_nav.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNav({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.black54,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Shops'),
+        BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: 'Order'),
+        BottomNavigationBarItem(icon: Icon(Icons.volunteer_activism), label: 'Volunteer'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
