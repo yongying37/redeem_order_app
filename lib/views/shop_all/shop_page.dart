ff4b62ed7a9196ce11034161c18e1e1306115750
2402212-zixin <2402212@sit.singaporetech.edu.sg>
2025-06-05 00:03:35 +0800

    Create shop all stalls folder and files

---
+++ lib/views/shop_all/shop_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'shop_layout.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ShopLayout(),
+    );
+  }
+}
