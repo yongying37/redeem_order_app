3419db28c580948646ba6405d8cec2729f9dd0c1
yongying37 <yongying3773@gmail.com>
2025-07-15 23:50:35 +0800

    Code clean up

--- lib/widgets/cart_badge_icon.dart
+++ lib/widgets/cart_badge_icon.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:redeem_order_app/bloc/cart/cart_bloc.dart';
